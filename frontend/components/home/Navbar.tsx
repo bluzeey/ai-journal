28faf3b1391869a5e3131cc3e49147471910e014
Sahil Maheshwari <sahilm1711@gmail.com>
2024-12-21 22:12:15 +0530

    Update Navbar.tsx

--- frontend/components/home/Navbar.tsx
+++ frontend/components/home/Navbar.tsx
@@ -5,19 +5,28 @@ import Link from "next/link"; // Optional for theme switching
 export function Navbar() {
   // If you have a theme switcher
   return (
-    <nav className="relative rounded-full max-w-4xl mx-auto w-full z-10 ">
-      <div className="bg-transparent mt-10  w-full dark:bg-blue-800 py-4 px-8 shadow-md mx-auto -mt-2 rounded-full">
+    <nav className="relative bg-white opacity-30 rounded-full max-w-4xl mx-auto w-full z-10 ">
+      <div className=" opacity-100 mt-10  w-full  py-4 px-8 shadow-md mx-auto -mt-2 rounded-full">
         {" "}
         {/* Adjusted margin for overlapping effect */}
         <div className="flex w-full justify-around items-center">
           <div className="flex space-x-8">
-            <Link href="/features" className="text-white hover:underline">
+            <Link
+              href="/features"
+              className="z-10 text-blue-600 font-bold hover:underline"
+            >
               Features
             </Link>
-            <Link href="/how-it-works" className="text-white hover:underline">
+            <Link
+              href="/how-it-works"
+              className="text-blue-600 hover:underline"
+            >
               How it Works
             </Link>
-            <Link href="/testimonials" className="text-white hover:underline">
+            <Link
+              href="/testimonials"
+              className="text-blue-600 hover:underline"
+            >
               Testimonials
             </Link>
           </div>
