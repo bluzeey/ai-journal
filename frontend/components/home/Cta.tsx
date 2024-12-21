51b201c3acccfb078e150c944f74642a8d307c29
Sahil Maheshwari <sahilm1711@gmail.com>
2024-12-21 21:33:34 +0530

    Adding changes

--- frontend/components/home/Cta.tsx
+++ frontend/components/home/Cta.tsx
@@ -12,12 +12,12 @@ export default function Cta() {
       <div className="absolute inset-0 bg-[#031F40] opacity-60" />{" "}
       {/* Dark overlay for better text visibility */}
       <div className="relative z-10 text-center text-white">
-        <h1 className="text-7xl text-center font-regular mb-4">
+        <h1 className="text-7xl max-w-5xl mx-auto text-center font-regular mb-4">
           Start Your AI-Powered Journaling Journey
-          <strong>Today</strong>
+          <span className="text-blue-300"> Today</span>
         </h1>
 
-        <p className="text-3xl max-w-3xl text-center mb-8">
+        <p className="text-3xl max-w-3xl mx-auto text-center mb-8">
           Unlock new insights about yourself and improve your mental well-being
           with our innovative AI Journal.
         </p>
