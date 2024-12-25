7e086e12c0354cec6a2de123c73467efc810d213
Sahil Maheshwari <sahilm1711@gmail.com>
2024-12-25 13:31:37 +0530

    Adding markdown for correct formatting

--- frontend/components/journal/Insights.tsx
+++ frontend/components/journal/Insights.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import ReactMarkdown from "react-markdown";
 import { Card } from "@/components/ui/card";
 
 interface InsightsProps {
@@ -10,7 +11,7 @@ export default function AIInsights({ insights }: InsightsProps) {
   return (
     <Card className="mt-4 p-6">
       <h2 className="text-xl font-bold">AI Insights</h2>
-      <p className="mt-2 text-xl">{insights}</p>
+      <ReactMarkdown className="mt-2 text-md">{insights}</ReactMarkdown>
     </Card>
   );
 }
