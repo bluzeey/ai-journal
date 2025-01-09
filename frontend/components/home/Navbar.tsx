aa87c6e2c4b1ee8d3bc9e7c4a8145e46d1ba67a2
Sahil Maheshwari <sahilm1711@gmail.com>
2025-01-09 18:21:36 +0530

    Adding changes

--- frontend/components/home/Navbar.tsx
+++ frontend/components/home/Navbar.tsx
@@ -2,34 +2,35 @@
 
 import Link from "next/link";
 import Logo from "@/app/images/logo.png";
-import Image from "next/image"; // Optional for theme switching
+import Image from "next/image";
 import { useState, useEffect } from "react";
 import { MenuIcon, XIcon } from "lucide-react";
 
-export function Navbar() {
+interface NavbarProps {
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
+}
+
+export function Navbar({ isOpen, setIsOpen }: NavbarProps) {
   const [isMobile, setIsMobile] = useState(false);
-  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
-    // This will run only on the client
     function handleResize() {
       setIsMobile(window.innerWidth < 768);
     }
 
-    if (typeof window !== "undefined") {
-      // Set initial value when component is mounted
-      handleResize();
+    // Set initial value when component is mounted
+    handleResize();
 
-      // Add event listener
-      window.addEventListener("resize", handleResize);
+    // Add event listener to handle window resizing
+    window.addEventListener("resize", handleResize);
 
-      // Clean up event listener on unmount
-      return () => window.removeEventListener("resize", handleResize);
-    }
+    // Clean up listener on component unmount
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return (
-    <nav className="relative bg-white bg-opacity-30 rounded-full max-w-4xl mx-auto w-full z-10 ">
+    <nav className="relative bg-white bg-opacity-30 rounded-full max-w-4xl mx-auto w-full z-10">
       <div className="mt-10 w-full py-4 px-8 shadow-md mx-auto -mt-2 rounded-full">
         <div className="flex w-full justify-between items-center">
           <Image
@@ -58,7 +59,7 @@ export function Navbar() {
                 Testimonials
               </Link>
               <Link href="/journal">
-                <button className="bg-[#2A5285] bg-opacity-75  border-2 border-white text-white hover:bg-gray-200 py-2 px-5 rounded-lg mx-2">
+                <button className="bg-[#2A5285] bg-opacity-75 border-2 border-white text-white hover:bg-gray-200 py-2 px-5 rounded-lg mx-2">
                   Start Journaling
                 </button>
               </Link>
@@ -66,7 +67,7 @@ export function Navbar() {
           )}
         </div>
         {isOpen && (
-          <div className="absolute top-0 left-0 w-full mt-20 bg-white bg-opacity-50 rounded-lg w-4/5">
+          <div className="absolute top-0 left-0 w-full mt-20 bg-white bg-opacity-50 z-20 rounded-lg">
             <div className="relative flex flex-col items-center space-y-4 p-4">
               <Link
                 href="/features"
@@ -81,7 +82,7 @@ export function Navbar() {
                 Testimonials
               </Link>
               <Link href="/journal">
-                <button className="bg-[#2A5285] bg-opacity-75  border-2 border-white text-white hover:bg-gray-200 py-2 px-5 rounded-lg mx-2">
+                <button className="bg-[#2A5285] bg-opacity-75 border-2 border-white text-white hover:bg-gray-200 py-2 px-5 rounded-lg mx-2">
                   Start Journaling
                 </button>
               </Link>
@@ -92,5 +93,3 @@ export function Navbar() {
     </nav>
   );
 }
-
-export default Navbar;
